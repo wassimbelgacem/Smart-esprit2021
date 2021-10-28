810fd4bdb4e9923cee586bc088f1a3f939fa0a80
maryem19 <maryem.awaidia@esprit.tn>
2021-10-28 14:00:37 +0100

    MAJ main

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include "fonction.h"
-int main() 
-{
-	int a;
-	int b;
-	printf("\nEntry First Number");
-	scanf("%d",a);
-	do {
-	printf("\nEnter the Second Number:");
-	scanf("%d",b);
-	} while(b == 0);
-	printf("\n%d+%d=%d",a,b,summer(a,b));
-	printf("\n%d-%d=%d",a,b,subtract(a,b));
-	printf("\n%d/%d=%f\n",a,b,divide(a,b));
-	return(0);
+int main()
+{int a ;
+int b ;
+a=10;
+b=3;
+printf("a=%d b=%d",a,b) ;
 }
 
